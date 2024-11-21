4f4efdf050e26aebd6a8d7e9c441647160c2b085
chriswilder3 <chriswilderdelta19@gmail.com>
2024-11-21 23:59:59 +0530

    adawd

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const sampleELem = <table>
+                        <th> <td>Name</td> <td>Age</td></th>
+                        <tr> <td>harry </td> <td> 40</td></tr>
+                        <tr> <td>andy </td> <td> 23</td></tr>
+                        <tr> <td>havel </td> <td> 34</td></tr>
+                    </table>
+root.render(sampleELem);
